86a95475440defd0b1ea553314f715feda0ba287
naekit <61828663+naekit@users.noreply.github.com>
2022-12-06 20:12:30 -0500

    optimized with a for loop

--- day6/resolver.js
+++ day6/resolver.js
@@ -4,26 +4,14 @@ const data = require('./data')
 let i = 0
 let charLeng = false
 do {
-    let charArr = [
-        data[i], 
-        data[i + 1], 
-        data[i + 2], 
-        data[i + 3],
-        data[i + 4], 
-        data[i + 5], 
-        data[i + 6],
-        data[i + 7], 
-        data[i + 8], 
-        data[i + 9],
-        data[i + 10], 
-        data[i + 11], 
-        data[i + 12],
-        data[i + 13]
-    ]
+    let charArr = []
+    for(let j = i; j < i + 14; j++){
+        charArr.push(data[j])
+    }
     let charSet = new Set(charArr)
 
     if([...charSet].length === 14){
-        console.log(i)
+        console.log(i + 14)
         charLeng = true
     }
     i++
